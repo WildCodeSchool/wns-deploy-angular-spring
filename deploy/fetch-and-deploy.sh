1be6ed47910602614eb2d96bcaeee445fc98a301
lgrignon <louis.grignon@protonmail.com>
2023-07-07 10:42:19 +0200

    fetch and deploy script

--- deploy/fetch-and-deploy.sh
+++ deploy/fetch-and-deploy.sh
@@ -1,1 +1,21 @@
-TODO
+#!/bin/bash
+
+export DOCKER_IMAGE_TAG="$1"
+export DOCKER_USER="$2"
+
+echo "docker image tag: $DOCKER_IMAGE_TAG pushed by $DOCKER_USER"
+
+if [ "$DOCKER_IMAGE_TAG" == "main"  ]; then 
+  export APP_ENV='prod' 
+  export GATEWAY_PORT=8000 
+else
+  export APP_ENV='preprod' 
+  export GATEWAY_PORT=8002 
+fi	
+
+COMPOSE_FILE=docker-compose.yml
+
+docker compose -f $COMPOSE_FILE down && \
+    docker compose -f $COMPOSE_FILE pull && \
+    docker compose -f $COMPOSE_FILE up -d;
+
